887ef65e59e34d43d87e3af9f6c528348e3f4ce0
Mani-sh27 <beingahuman2512gmail.com>
2025-05-08 16:10:32 +0530

    auto push

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)  # headless = no GUI
-    page = browser.new_page()
-    page.goto("https://example.com")
-    
-    # Extract text from a specific element
-    heading = page.locator("h1").text_content()
-    print("Page Heading:", heading)
-
+    browser = p.chromium.launch(
+        headless=True,
+        args=["--disable-http2", "--no-sandbox", "--disable-blink-features=AutomationControlled"]
+    )
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+    )
+    page = context.new_page()
+    page.goto("https://www.justdial.com/")
+    print("Page title:", page.title())
     browser.close()
